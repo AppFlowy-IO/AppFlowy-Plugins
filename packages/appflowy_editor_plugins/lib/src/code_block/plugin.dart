45aef9148f361b4ccf07d29b51b663110eaca73b
Mathias Mogensen <mathias@appflowy.io>
2024-04-16 11:39:47 +0200

    fix: export localization helper

--- packages/appflowy_editor_plugins/lib/src/code_block/plugin.dart
+++ packages/appflowy_editor_plugins/lib/src/code_block/plugin.dart
@@ -3,3 +3,4 @@ export 'code_block_block_component.dart';
 export 'code_block_shortcuts.dart';
 export 'code_block_style.dart';
 export 'code_block_themes.dart';
+export 'code_block_localization.dart';
