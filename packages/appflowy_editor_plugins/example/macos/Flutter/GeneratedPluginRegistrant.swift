58c436244788308d68ec56da3dc7d71b868a98d9
Richard Shiue <71320345+richardshiue@users.noreply.github.com>
2026-01-13 14:57:00 +0800

    chore: code block margins

--- packages/appflowy_editor_plugins/example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/appflowy_editor_plugins/example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,11 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import device_info_plus
-import path_provider_foundation
+import file_picker
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
